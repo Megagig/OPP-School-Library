ca305bd174e104ae20f451d24c30b89b2c451ef1
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-09 19:47:56 +0100

    method to list all books

--- app.rb
+++ app.rb
@@ -5,11 +5,15 @@ require relative 'classroom'
 require relative 'rental'
 
 class App
-    attr_accessor :books, :people, :rentals
+    # attr_accessor :books, :people, :rentals
   
     def initialize
       @people = []
       @books = []
       @rentals = []
     end
+
+    def list_all_books
+        @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+      end  
 end
