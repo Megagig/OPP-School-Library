28171b5542c411c8b90d07b9cc4d5f8998bd8e2c
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-09 20:25:34 +0100

    update student_class.rb

--- student_class.rb
+++ student_class.rb
@@ -3,7 +3,7 @@ require_relative 'person_class'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+  def initialize(classroom, age, parent_permission, name = 'unknown')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
