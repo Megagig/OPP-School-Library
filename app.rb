d98cfbf62697f1d8c077a1dff1bbcb86cf979791
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-09 20:02:23 +0100

    create a student,teacher and list books and people

--- app.rb
+++ app.rb
@@ -72,5 +72,30 @@ class App
 
       private
 
+      def create_a_student(name, age)
+        print 'Has parent permission? [Y/N]: '
+        parent_permission = %w[Y y].include?(gets.chomp)
+    
+        label = 'math'
+        classroom = Classroom.new(label)
+        Student.new(classroom, age, parent_permission, name)
+      end
+    
+      def create_a_teacher(name, age)
+        print 'Specialization: '
+        specialization = gets.chomp
+        Teacher.new(specialization, age, name)
+      end
+    
+      def list_books
+        @books.each_with_index { |book, index| puts "#{index}) Title \"#{book.title}\", Author: #{book.author}" }
+      end
+    
+      def list_people
+        @people.each_with_index do |person, index|
+          puts "#{index}) [#{person.type}] Name: #{person.name} ID: #{person.id}, Age: #{person.age}"
+        end
+      end
+
 
 end
